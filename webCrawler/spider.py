a28f8e30374d6264c9ff37ce6741fd43334fe643
RobByrnes <robbyrnes@hotmail.co.uk>
2021-03-09 22:19:12 +0000

    Spider completed and working a treat!

--- webCrawler/spider.py
+++ webCrawler/spider.py
@@ -8,46 +8,73 @@ from savedata import SaveData
 class Spider:
     def __init__(self, url, name):
         self.TARGET_URL = url
+        self.domain = self.get_domain_name(url)
         self.PROJECT_NAME = name
-        self.queued_links_file = self.PROJECT_NAME + "/queued_links.txt"
-        self.links_visited_file = self.PROJECT_NAME + "/visited_links.txt"
+        self.queued_links_file = name + "\\queued_links.txt"
+        self.links_visited_file = name + "\\visited_links.txt"
+        self.Saver = SaveData()
         self.queue = set()
         self.crawled = set()
-        self.search()
-        self.Saver = SaveData()
+        self.setup()
+        self.search(spider_name="Charlotte the Spider", url=url)
 
-    def search(self, url=None):
-        if url is None:
-            url = self.TARGET_URL
-        href_links = self.extract_links(url)
-        for link in href_links:
-            link = urlparse.urljoin(url, link)
-            if "#" in link:
-                link = link.split("#")[0]
-            self.sort_to_queue(link)
-            self.add_to_crawled_set(link)
+    def setup(self):
+        self.Saver.create_dir(self.PROJECT_NAME)
+        self.Saver.create_files(self.PROJECT_NAME, self.TARGET_URL)
+        self.queue = self.Saver.file_to_set(self.queued_links_file)
+        self.crawled = self.Saver.file_to_set(self.links_visited_file)
 
-    def extract_links(self, url):
+    def search(self, spider_name, url):
+        try:
+            if url not in self.crawled:
+                self.sort_to_queue(spider_name, Spider.extract_links(url))
+            if url in self.queue:
+                self.queue.remove(url)
+            self.update()
+        except Exception as e:
+            print("[-] Exception occured in " + url + " >> Exception: " + str(e))
+
+    @staticmethod
+    def extract_links(url):
         try:
             response = requests.get(url)
             return re.findall('(?:href=")(.*?)"', response.content.decode(errors="ignore"))
-        except requests.exceptions.ConnectionError:
-            pass
-        except requests.exceptions.MissingSchema:
-            pass
+        except Exception as e:
+            print("[-] Exception occured in " + url + " >> Exception: " + str(e))
 
-    def sort_to_queue(self, link):
-        if (link not in self.crawled) or (link not in self.queue):
-            self.queue.add(link)
+    def sort_to_queue(self, spider_name, links):
 
-    def add_to_crawled_set(self, link):
-        if link not in self.crawled:
+        for link in links:
+            if (link in self.crawled) or (link in self.queue):
+                continue
+            if self.domain == Spider.get_domain_name(link):
+                link = urlparse.urljoin(self.TARGET_URL, link)
+                if "#" in link:
+                    link = link.split("#")[0]
+                print("[+] now crawling >> " + link + " with " + spider_name)
+                print("[+] Queued " + str(len(self.queue)) + " >> Crawled " + str(len(self.crawled)))
+                self.queue.add(link)
+                self.add_to_crawled(link)
+
+    def add_to_crawled(self, link):
+        if (link not in self.crawled) and (self.TARGET_URL in link):
             self.crawled.add(link)
 
     def update(self):
         self.Saver.set_to_file(self.queue, self.queued_links_file)
         self.Saver.set_to_file(self.crawled, self.links_visited_file)
 
+    @staticmethod
+    def get_domain_name(url):
+        try:
+            results = str(Spider.get_sub_domain_name(url).split('.'))
+            return results[-2] + '.' + results[-1]
+        except Exception as e:
+            return str(e)
 
-
-
+    @staticmethod
+    def get_sub_domain_name(url):
+        try:
+            return urlparse.urlparse(url).netloc
+        except Exception as e:
+            return str(e)
--- webCrawler/spider_main.py
+++ webCrawler/spider_main.py
@@ -2,50 +2,47 @@
 
 import threading
 from spider import Spider
-from savedata import SaveData
+from savedata import *
 from queue import Queue
 
 
 class SpiderMain:
     def __init__(self):
-        self.TARGET_URL = "https://www.skatewarehouse.co.uk"
-        self.PROJECT_NAME = "skatewarehouse"
+        self.TARGET_URL = "https://www.creditoncoffee.co.uk"
+        self.PROJECT_NAME = "ccc"
+        self.queue_path = self.PROJECT_NAME + "/queued_links.txt"
         self.WORKERS = 4
         self.Spider = Spider(self.TARGET_URL, self.PROJECT_NAME)  # crawling object
-        self.Saver = SaveData()  # saving object
         self.QUEUE = Queue()  # queue object
 
-    def init_files(self, name, url):  # using saver object to create dir and files
-        self.Saver.create_dir(name)
-        self.Saver.create_files(name, url)
-
     def spawn_spiders(self):  # creates workers using threading lib
-        print("[+] Creating workforce of " + str(self.WORKERS) + " spiders >>")
+        print("[+] Creating workforce of " + str(self.WORKERS) + " more spiders >>")
         for _ in range(self.WORKERS):
             thread = threading.Thread(target=self.accept_job)
-            thread.daemon = False
+            thread.daemon = True
             thread.start()
 
     def accept_job(self):
-        link = self.QUEUE.get()  # take a job from the queue
-        self.Spider.search(link)  # crawl this link
-        self.QUEUE.task_done()  # indicate job is complete
+        while True:
+            link = self.QUEUE.get()  # take a job from the queue
+            self.Spider.search(threading.current_thread().name, link)  # crawl this link
+            self.QUEUE.task_done()  # indicate job is complete
 
     def crawl(self):
-        links_queue = self.Saver.file_to_set(self.Saver.queued_links_path)
-        if len(links_queue) > 0:
-            print("[+] " + str(len(links_queue)) + " queued links awaiting spiders >>")
-        self.add_job()
+        links_queue = SaveData.file_to_set(self.queue_path)
+        for link in links_queue:
+            if len(links_queue) > 0:
+                if str(self.TARGET_URL) in str(link):
+                    print("[+] " + str(len(links_queue)) + " queued links awaiting spiders >>")
+                    self.add_job()
 
     def add_job(self):
-        for link in self.Saver.file_to_set(self.Saver.queued_links_path):
+        for link in SaveData.file_to_set(self.queue_path):
             self.QUEUE.put(link)
         self.QUEUE.join()
         self.crawl()
 
     def main(self):
-        self.init_files(self.PROJECT_NAME, self.TARGET_URL)
-
         self.spawn_spiders()
 
         self.crawl()
